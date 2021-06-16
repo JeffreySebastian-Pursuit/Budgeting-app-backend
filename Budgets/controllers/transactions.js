7fb92d0d9c9bbed2fd1b39369758bef461c6db5a
Jeffrey <jeffreysebastian@pursuit.org>
2021-06-15 21:54:32 -0400

    Created Backend

---
+++ Budgets/controllers/transactions.js
@@ -0,0 +1,42 @@
+const transactions = require("express").Router();
+const { rawListeners } = require("../../app");
+const dataArr = require("../models/data");
+
+// get all data or Index
+transactions.get("/", (req, res) => {
+  res.json(dataArr);
+});
+
+// Show: get an specific id or entry
+transactions.get("/:id", (req, res) => {
+  const data = dataArr[req.params.id];
+  if (data) {
+    res.json(data);
+  } else {
+    res.redirect("/404");
+  }
+});
+
+// POST
+transactions.post("/", (req, res) => {
+  const { body } = req;
+  dataArr.push(body);
+  const newData = dataArr.length - 1;
+  res.json(dataArr[newData]);
+});
+
+// PUT
+transactions.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  dataArr[id] = body;
+  res.json(dataArr[id]);
+});
+
+//  Delete
+transactions.delete("/:id", (req, res) => {
+  const deletedData = dataArr.splice(req.params.id, 1);
+  res.json(deletedData[0]);
+});
+
+module.exports = transactions;
---
+++ Budgets/models/data.js
@@ -0,0 +1,18 @@
+module.exports = [
+  { id: 0, from: "Corey", date: "April 10", name: "Code Along", amount: 10 },
+  { id: 1, from: "Corey", date: "April 7", name: "AirPods", amount: 200 },
+  { id: 2, from: "Corey", date: "April 9", name: "Apple", amount: -53 },
+  { id: 3, from: "Corey", date: "April 28", name: "Taxes", amount: 123 },
+  { id: 4, from: "Gigi", date: "April 19", name: "One Thing", amount: 40 },
+  { id: 5, from: "Gigi", date: "April 1", name: "Dog Food", amount: 20 },
+  { id: 6, from: "Gigi", date: "April 3", name: "Car Wash", amount: 13 },
+  { id: 7, from: "Gigi", date: "April 8", name: "Electric Bill", amount: 231 },
+  { id: 8, from: "Tim", date: "May 20", name: "Groceries", amount: 30 },
+  { id: 9, from: "Tim", date: "May 20", name: "New Light Bulbs", amount: 10 },
+  { id: 10, from: "Tim", date: "May 17", name: "Hair Cut", amount: 25 },
+  { id: 11, from: "Tim", date: "May 17", name: "Venmo", amount: 109 },
+  { id: 12, from: "Mashu", date: "May 19", name: "Fruit", amount: -10 },
+  { id: 13, from: "Mashu", date: "May 19", name: "BBQ", amount: 50 },
+  { id: 14, from: "Mashu", date: "May 27", name: "Taxi", amount: 38 },
+  { id: 15, from: "Mashu", date: "May 27", name: "Github", amount: 7 },
+];
