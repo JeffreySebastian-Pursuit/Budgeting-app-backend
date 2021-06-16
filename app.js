a648695d208fc519cd4bbd0a63f442e6a7b7c256
Jeffrey <jeffreysebastian@pursuit.org>
2021-06-15 21:55:31 -0400

    Created Backend

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require('cors');
+const transactionController = require("./Budgets/controllers/transactions")
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use((req, res, next) => {
+  next();
+});
+
+app.use("/transactions", transactionController)
 
 // ROOT
 app.get("/", (req, res) => {
   res.send("Basic Express App - ROOT");
 });
 
+// Error message
+app.get("*", (req, res) => {
+  res.status(404).send("Page Not Found!");
+});
 module.exports = app;
